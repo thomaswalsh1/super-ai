0ac3486648782b4223959b618a20924effda72df
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2024-11-09 00:08:37 -0500

    Add logo and ui boxes
    Co-authored-by: thomaswalsh1 <thomaswalsh1@users.noreply.github.com>
    Co-authored-by: Kunal Soni <Kunnaalsoni@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-row min-h-svh" id="bottom-container">
+    <div class="basis-1/2 border p-6 flex items-end" id="prompt-half">
+        <div id="prompt box">
+            prompt box
+        </div>
+    </div>
+    <div class="basis-1/2 border p-6" id="output-half">
+        output
+    </div>
+</div>
