92f70a40de13acaad953093c156cd990a9408401
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2024-11-09 00:11:02 -0500

    Add text input component
    Co-authored-by: thomaswalsh1 <thomaswalsh1@users.noreply.github.com>
    Co-authored-by: Kunal Soni <Kunnaalsoni@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
+<script lang="ts">
+    import { Input } from "$lib/components/ui/input";
+</script>
+
 <div class="flex flex-row min-h-svh" id="bottom-container">
     <div class="basis-1/2 border p-6 flex items-end" id="prompt-half">
         <div id="prompt box">
-            prompt box
+            <Input type="text" placeholder="chat" class="max-w-xs" />
         </div>
     </div>
     <div class="basis-1/2 border p-6" id="output-half">
