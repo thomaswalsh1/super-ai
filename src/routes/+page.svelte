4181daafe9d31ec440ddcaa4e85c111f366b7a9f
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2024-11-09 00:47:05 -0500

    Add textarea and tabs components
    Co-authored-by: thomaswalsh1 <thomaswalsh1@users.noreply.github.com>
    Co-authored-by: Kunal Soni <Kunnaalsoni@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,32 @@
 <script lang="ts">
-    import { Input } from "$lib/components/ui/input";
+    import { Textarea } from "$lib/components/ui/textarea";
     import { Button } from "$lib/components/ui/button";
+    import * as Tabs from "$lib/components/ui/tabs";
 </script>
 
-<div class="flex flex-row h-[42rem]" id="bottom-container">
-    <div class="basis-1/2 border p-6 flex items-center" id="prompt-half">
-        <div id="prompt box" class="flex basis-5/6 gap-4">
-            <Input type="text" placeholder="chat" class="max-w-xs" />
+<div class="flex flex-row h-full" id="bottom-container">
+    <div class="basis-1/2 border p-6 flex items-start w-full" id="prompt-half">
+        <div id="prompt box" class="flex gap-4 w-full">
+            <Textarea placeholder="chat" class="" />
             <Button type="submit">Send</Button>
         </div>
     </div>
     <div class="basis-1/2 border p-6" id="output-half">
-        output
+        <Tabs.Root value="LLM 1" class="w-[400px]">
+            <Tabs.List>
+                <Tabs.Trigger value="LLM 1">LLM 1</Tabs.Trigger>
+                <Tabs.Trigger value="LLM 2">LLM 2</Tabs.Trigger>
+                <Tabs.Trigger value="LLM 3">LLM 3</Tabs.Trigger>
+            </Tabs.List>
+            <Tabs.Content value="LLM 1">
+                Here is LLM 1
+            </Tabs.Content>
+            <Tabs.Content value="LLM 2">
+                Here is LLM 2
+            </Tabs.Content>
+            <Tabs.Content value="LLM 3">
+                Here is LLM 3
+            </Tabs.Content>
+        </Tabs.Root>
     </div>
 </div>
