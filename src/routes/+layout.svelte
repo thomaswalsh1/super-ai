8f8ffec4e3d0ea6fea0e0eb3faebb57a9d46e442
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2024-11-08 23:24:50 -0500

    Add mode-watcher
    Co-authored-by: thomaswalsh1 <thomaswalsh1@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import "../app.css";
+	import { ModeWatcher } from "mode-watcher";
 	import Header from "./Header.svelte";
+
 	let { children } = $props();
 </script>
+
+<ModeWatcher />
 
 <Header />
 
