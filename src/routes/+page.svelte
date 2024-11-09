0a364ea3c91840075f428bd19d888e0c532bdafe
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2024-11-09 01:34:04 -0500

    Add output box component
    Co-authored-by: thomaswalsh1 <thomaswalsh1@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,30 +2,31 @@
     import { Textarea } from "$lib/components/ui/textarea";
     import { Button } from "$lib/components/ui/button";
     import * as Tabs from "$lib/components/ui/tabs";
+    import OutputBox from "./OutputBox.svelte";
 </script>
 
 <div class="grow flex flex-row" id="bottom-container">
     <div class="basis-1/2 border p-6 flex items-start w-full" id="prompt-half">
         <div id="prompt box" class="flex gap-4 w-full">
-            <Textarea placeholder="chat" class="" />
+            <Textarea placeholder="chat" class="bg-muted text-lg" />
             <Button type="submit">Send</Button>
         </div>
     </div>
     <div class="basis-1/2 border p-6" id="output-half">
-        <Tabs.Root value="LLM 1" class="w-[400px]">
+        <Tabs.Root value="LLM 1" class="">
             <Tabs.List>
                 <Tabs.Trigger value="LLM 1">LLM 1</Tabs.Trigger>
                 <Tabs.Trigger value="LLM 2">LLM 2</Tabs.Trigger>
                 <Tabs.Trigger value="LLM 3">LLM 3</Tabs.Trigger>
             </Tabs.List>
             <Tabs.Content value="LLM 1">
-                Here is LLM 1
+                <OutputBox outputText="text 1" />
             </Tabs.Content>
             <Tabs.Content value="LLM 2">
-                Here is LLM 2
+                <OutputBox outputText="text 2" />
             </Tabs.Content>
             <Tabs.Content value="LLM 3">
-                Here is LLM 3
+                <OutputBox outputText="text 3" />
             </Tabs.Content>
         </Tabs.Root>
     </div>
---
+++ src/routes/OutputBox.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    let { outputText } = $props();
+</script>
+<div id="output-box" class="flex h-full bg-muted p-6">
+    {outputText}
+    blach
+</div>
